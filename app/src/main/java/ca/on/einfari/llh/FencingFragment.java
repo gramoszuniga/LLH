809f110cd1a7300790f9a797316d4c525f8aaa07
Gonzalo Ramos Zúñiga <gramoszuniga5711@conestogac.on.ca>
2017-09-22 00:44:40 -0500

    Tabs in main activity + some clean up

---
+++ app/src/main/java/ca/on/einfari/llh/FencingFragment.java
@@ -0,0 +1,16 @@
+package ca.on.einfari.llh;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FencingFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_fencing, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/ca/on/einfari/llh/SidingFragment.java
@@ -0,0 +1,16 @@
+package ca.on.einfari.llh;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SidingFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_siding, container, false);
+        return rootView;
+    }
+}
